8b0b1134eaeeed89e2012a36b03db4b40a734897
Raphael Alves <rsaraphael@gmail.com>
2018-04-10 18:32:45 -0300

    Basic setup of project

---
+++ src/main/java/com/rsaraphael/flightpanel/FlightPanelApplication.java
@@ -0,0 +1,12 @@
+package com.rsaraphael.flightpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPanelApplication.class, args);
+	}
+}
