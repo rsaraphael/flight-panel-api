87ab3e516e90044cb9f0c41ac13a276c05648ac8
Raphael Alves <rsaraphael@gmail.com>
2018-04-10 23:30:20 -0300

    Fix sql

--- src/main/java/com/rsaraphael/flightpanel/itinerary/Itinerary.java
+++ src/main/java/com/rsaraphael/flightpanel/itinerary/Itinerary.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rsaraphael.flightpanel.flight.Flight;
 
 import lombok.Getter;
--- src/main/resources/db-scripts/00-create-tables.sql
+++ src/main/resources/db-scripts/00-create-tables.sql
@@ -15,15 +15,15 @@ CREATE TABLE Aircraft (
 
 CREATE TABLE Pilot (
     id        serial primary key,
-    name       varchar(100)
+    name      varchar(100)
 );
 
 
 CREATE TABLE Location (
     id        serial primary key,
     city       varchar(100),
-    state       varchar(100)
-    country       varchar(100)
+    state      varchar(100),
+    country    varchar(100)
 );
 
 CREATE TABLE Itinerary (
