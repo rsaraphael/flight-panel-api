bab47188a0af2c6cc3d2e40277b244064145e138
Raphael Alves <rsaraphael@gmail.com>
2018-04-12 20:36:28 -0300

    Add new endpoint to request flight

--- src/main/java/com/rsaraphael/flightpanel/restapi/FlightController.java
+++ src/main/java/com/rsaraphael/flightpanel/restapi/FlightController.java
@@ -5,19 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rsaraphael.flightpanel.flight.FlightDto;
 import com.rsaraphael.flightpanel.flight.FlightService;
-import com.rsaraphael.flightpanel.flight.FlightVO;
 
-@RestController(value="/flight")
+@RestController
+@RequestMapping(value="/flight")
 @CrossOrigin
 public class FlightController {
 	@Autowired
 	private FlightService flightService;
 
-	@GetMapping()
-	public List<FlightVO> getAllFlights() {
-		return flightService.getAllFlights();
+	@GetMapping
+	public List<Long> getAllFlightNumbers() {
+		return flightService.getAllFlightNumber();
+	}
+	
+	@GetMapping(value="/{id}")
+	public FlightDto getFlight(@PathVariable Long id) {
+		return flightService.getFlightById(id);
 	}
 }
