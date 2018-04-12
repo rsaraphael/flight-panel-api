fc9088ea653cb12c07b2d5e9ff6ab9ff9a35aecb
Raphael Alves <rsaraphael@gmail.com>
2018-04-12 20:37:16 -0300

    Add method to handle list of flight numbers and a specific one

--- src/main/java/com/rsaraphael/flightpanel/flight/FlightService.java
+++ src/main/java/com/rsaraphael/flightpanel/flight/FlightService.java
@@ -1,5 +1,6 @@
 package com.rsaraphael.flightpanel.flight;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,15 @@ public class FlightService {
 	@Autowired
 	private FlightAdapter flightAdapter;
 
-	public List<FlightVO> getAllFlights() {
-		return flightAdapter.adapt(flightRepository.findAll());
+	public List<Long> getAllFlightNumber() {
+		List<Long> flightNumbers = new ArrayList<>();
+		List<Flight> flights = flightRepository.findAll();
+		flights.forEach(flight -> flightNumbers.add(flight.getId()));
+		return flightNumbers;
+	}
+
+	public FlightDto getFlightById(Long id) {
+		return flightAdapter.adapt(flightRepository.findById(id).get());
 	}
 
 }
