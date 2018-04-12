ffc36448c67b64bfa62ea0c1c76c6c29d5228019
Raphael Alves <rsaraphael@gmail.com>
2018-04-12 20:31:17 -0300

    Add basic setup for tests

--- src/test/java/com/rsaraphael/flightpanel/FlightPanelApplicationTests.java
+++ src/test/java/com/rsaraphael/flightpanel/FlightPanelApplicationTests.java
@@ -3,21 +3,21 @@ package com.rsaraphael.flightpanel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.rsaraphael.flightpanel.config.DatabaseBean;
 import com.rsaraphael.flightpanel.config.FlightPanelApplication;
-import com.rsaraphael.flightpanel.flight.Flight;
 import com.rsaraphael.flightpanel.flight.FlightRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes= FlightPanelApplication.class)
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @DataJpaTest
 @Import(DatabaseBean.class)
 public class FlightPanelApplicationTests {
@@ -26,10 +26,8 @@ public class FlightPanelApplicationTests {
 	private FlightRepository flightRepository;
 
 	@Test
+	@Sql(scripts="classpath:db-scripts/03-inserts.sql")
 	public void contextLoads() {
-		Flight flight = new Flight();
-		flight.setId(1l);
-		flightRepository.save(flight);
 		flightRepository.findAll();
 	}
 
